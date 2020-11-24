61ab129fc78c3ff22408deed8235697c9a4c4bf9
Williams Fuentes <williamsfuentes@gmail.com>
2020-11-24 19:04:12 -0300

    clase 5

---
+++ src/es7/index.js
@@ -0,0 +1,21 @@
+//------------------------------------
+
+//include
+
+let numbers = [1,2,4,5,7,9];
+
+if(numbers.includes(7)){
+    console.log('Si se encuentra el valor 7')
+}
+else{
+    console.log('no se encuentra el valor 7')
+}
+//------------------------------------
+
+//como elevar a la potencia
+
+let base = 4;
+let exponente= 3;
+let result = base ** exponente;
+console.log(result);
+//------------------------------------
