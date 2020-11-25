14005d833dadae0f49d9b1bfbb3561d08f320ae8
Williams Fuentes <williamsfuentes@gmail.com>
2020-11-24 22:51:50 -0300

    clase 7

--- src/es8/index.js
+++ src/es8/index.js
@@ -43,5 +43,36 @@ const obj = {
     name: 'oscar', //el trailin coma es que puedo poner una comar al final y no explota
 }
 //------------------------------------
+
+// async-await
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        //(false)
+        ? setTimeout(() => resolve('HelloWorld'), 3000)
+        : reject(new Error ('Test Error'))
+    })
+} 
+
+const helloAsync = async () =>{
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+anotherFunction();
 //------------------------------------
+//leer el articulo de abajo porfavor
 //------------------------------------
+//https://platzi.com/tutoriales/1789-asincronismo-js/5063-las-promesas-y-async-await-logicamente-no-son-iguales-y-te-explico-el-porque/
